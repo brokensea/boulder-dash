33836d39a25bef080eea3d16a3e20c0551fec429
heidelme <heidelme@students.uni-marburg.de>
2016-05-21 22:04:17 +0200

    Introduce constants (manual work) 9

--- src/fr/enssat/BoulderDash/models/MagicWallModel.java
+++ src/fr/enssat/BoulderDash/models/MagicWallModel.java
@@ -15,15 +15,6 @@ import fr.enssat.BoulderDash.models.DisplayableElementModel;
  * @since       2015-06-19
  */
 public class MagicWallModel extends DisplayableElementModel {
-	private static String spriteName;
-	private static boolean isDestructible;
-	private static boolean canMove;
-	private static boolean impactExplosive;
-	private static boolean animate;
-	private static int priority;
-	private static boolean falling;
-	private static String collideSound;
-
     /**
      * Stores the frames
      * Used for the sprites
@@ -38,22 +29,20 @@ public class MagicWallModel extends DisplayableElementModel {
      * Static dataset
      * Specifies the physical parameters of the object
      */
-	static {
-		spriteName = "magicwall";
-		isDestructible = false;
-		canMove = false;
-		impactExplosive = false;
-		animate = false;
-		priority = 3;
-		falling = false;
-		collideSound = "touch";
-	}
+	private static final String SPRITE_NAME = "magicwall";
+	private static final boolean IS_DESTRUCTIBLE = false;
+	private static final boolean CAN_MOVE = false;
+	private static final boolean IMPACT_EXPLOSIVE = false;
+	private static final boolean ANIMATE = false;
+	private static final int PRIORITY = 3;
+	private static final boolean FALLING = false;
+	private static final String COLLIDE_SOUND = "touch";
 
     /**
      * Class constructor
      */
 	public MagicWallModel() {
-		super(new Properties(isDestructible, canMove, impactExplosive, animate, falling), spriteName, priority, collideSound);
+		super(new Properties(IS_DESTRUCTIBLE, CAN_MOVE, IMPACT_EXPLOSIVE, ANIMATE, FALLING), SPRITE_NAME, PRIORITY, COLLIDE_SOUND);
 		this.currentFrame = 0;
 		this.speed =  100;
 		this.initSprites();
