1216019e79585b5df5aafab752f386e74a14f99f
heidelme <heidelme@students.uni-marburg.de>
2016-05-21 17:12:27 +0200

    Benenne Felder einheitlich 3 (rename)

--- src/fr/enssat/BoulderDash/models/DisplayableElementModel.java
+++ src/fr/enssat/BoulderDash/models/DisplayableElementModel.java
@@ -62,9 +62,9 @@ public abstract class DisplayableElementModel {
 		this.collideSound = collideSound;
 	}
 
-    public DisplayableElementModel(boolean destructible, boolean moving, String spriteName, int priority, boolean impactExplosive, boolean animate, boolean falling, String collideSound) {
+    public DisplayableElementModel(boolean isDestructible, boolean canMove, String spriteName, int priority, boolean impactExplosive, boolean animate, boolean falling, String collideSound) {
         this(
-                destructible, moving, spriteName, priority, impactExplosive, animate, falling, collideSound, false
+                isDestructible, canMove, spriteName, priority, impactExplosive, animate, falling, collideSound, false
         );
     }
 
